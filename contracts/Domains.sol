664930076ed4d322f53cdc9c3182b6197e50f662
Yash Jagtap <jagtapyash2512@gmail.com>
2022-06-10 17:08:16 +0530

    setting the price

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -2,22 +2,47 @@
 
 pragma solidity ^0.8.0;
 
+import { StringUtils } from "./libraries/StringUtils.sol";
 import "hardhat/console.sol";
 
 contract Domains {
+  // for domain
+  string public tld; // top-level domain
   // data type to store their names
   mapping(string => address) public domains;
 
   // mapping to store values
   mapping(string => string) public records;
 
-  constructor() {
-    console.log("This is a constructor");
+  constructor(string memory _tld) payable {
+    tld = _tld;
+    console.log("%s name service deployed", _tld);
+  }
+
+  // this function will give us the price of a domain based on length
+  function price(string calldata name) public pure returns (uint) {
+    uint len = StringUtils.strlen(name);
+    require(len > 0);
+    if (len == 3) {
+      return 5 * 10**17; // 18 decimals, 0.5 MATIC
+    }
+    else if (len == 4) {
+      return 3 * 10**17; // 0.3
+    }
+    else {
+      return 1 * 10**17;
+    }
   }
   // a register function that adds their names to our mapping
-  function register(string calldata name) public { // calldata - this indicates the “location” of where the name argument should be stored
+  function register(string calldata name) public payable { // calldata - this indicates the “location” of where the name argument should be stored
     // check that name is unregistered
     require(domains[name] == address(0));
+
+    uint _price = price(name);
+
+    // check if enough MATIC was paid in txn
+    require(msg.value >= _price, "Not enough MATIC paid");
+
     domains[name] = msg.sender;
     console.log("%s has registered a domain!", msg.sender);
   }
