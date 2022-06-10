b9d62513f25f1493ebc6b108446090f347b1df0e
Yash Jagtap <jagtapyash2512@gmail.com>
2022-06-10 23:12:11 +0530

    added mumbai network

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,4 +18,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    mumbai: {
+      url: "ALCHEMY_URL",
+      accounts: ["PRIVATE_KEY"],
+    }
+  }
 };
