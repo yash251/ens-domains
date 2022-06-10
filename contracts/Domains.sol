d2a550e15a7a36ccc1404d7132fe561010874e1c
Yash Jagtap <jagtapyash2512@gmail.com>
2022-06-10 12:30:58 +0530

    register function basic

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -7,7 +7,18 @@ import "hardhat/console.sol";
 contract Domains {
   // data type to store their names
   mapping(string => address) public domains;
+
   constructor() {
     console.log("This is a constructor");
+  }
+  // a register function that adds their names to our mapping
+  function register(string calldata name) public {
+    domains[name] = msg.sender;
+    console.log("%s has registered a domain!", msg.sender);
+  }
+
+  // this function will give us domain owner's address
+  function getAddress(string calldata name) public view returns (address) {
+    return domains[name];
   }
 }
