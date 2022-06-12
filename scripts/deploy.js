b861979a511f2299d2d3affc0f858270341f3eef
Yash Jagtap <jagtapyash2512@gmail.com>
2022-06-12 18:36:53 +0530

    price = 0.3 for yash

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,7 +5,7 @@ const main = async () => {
 
     console.log("Contract deployed to:", domainContract.address);
 
-    let txn = await domainContract.register("yash", { value: hre.ethers.utils.parseEther("0.1") });
+    let txn = await domainContract.register("yash", { value: hre.ethers.utils.parseEther("0.3") });
     await txn.wait();
     console.log("Minted domain yash.khushi");
 
