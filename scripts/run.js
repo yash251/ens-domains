988b3ea0f7b8f9ff4c21e6335ae16bd389d08f87
Yash Jagtap <jagtapyash2512@gmail.com>
2022-06-13 13:32:40 +0530

    testing withdraw

--- scripts/run.js
+++ scripts/run.js
@@ -1,20 +1,36 @@
 const main = async () => {
+  const [owner, superCoder] = await hre.ethers.getSigners();
   const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-  // We pass in "ninja" to the constructor when deploying
-  const domainContract = await domainContractFactory.deploy("ninja");
+  const domainContract = await domainContractFactory.deploy("khushi");
   await domainContract.deployed();
 
-  console.log("Contract deployed to:", domainContract.address);
+  console.log("Contract owner:", owner.address);
 
-  // We're passing in a second variable - value. This is the moneyyyyyyyyyy
-  let txn = await domainContract.register("mortal",  {value: hre.ethers.utils.parseEther('0.1')});
+  let txn = await domainContract.register("a16z",  {value: hre.ethers.utils.parseEther('1234')});
   await txn.wait();
-
-  const address = await domainContract.getAddress("mortal");
-  console.log("Owner of domain mortal:", address);
 
   const balance = await hre.ethers.provider.getBalance(domainContract.address);
   console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
+
+  try {
+    txn = await domainContract.connect(superCoder).withdraw();
+    await txn.wait();
+  } catch(error){
+    console.log("Could not rob contract");
+  }
+
+  let ownerBalance = await hre.ethers.provider.getBalance(owner.address);
+  console.log("Balance of owner before withdrawal:", hre.ethers.utils.formatEther(ownerBalance));
+
+  txn = await domainContract.connect(owner).withdraw();
+  await txn.wait();
+  
+  // fetching balance of contract & owner
+  const contractBalance = await hre.ethers.provider.getBalance(domainContract.address);
+  ownerBalance = await hre.ethers.provider.getBalance(owner.address);
+
+  console.log("Contract balance after withdrawal:", hre.ethers.utils.formatEther(contractBalance));
+  console.log("Balance of owner after withdrawal:", hre.ethers.utils.formatEther(ownerBalance));
 }
 
 const runMain = async () => {
